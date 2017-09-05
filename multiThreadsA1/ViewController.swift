850a14430dcabd5d9353e7638bdde3d4e2a0d0f3
ta <ta@nius-mac-mini.local>
2017-09-05 18:07:39 -0500

    initial commit

--- multiThreadsA1/ViewController.swift
+++ multiThreadsA1/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var maxInt: UITextField!
+    
+    @IBOutlet weak var displayTableView: UITableView!
 
+    @IBAction func submitButton(_ sender: UIButton) {
+        let n = Int(maxInt.text!)
+        
+        // let sqrtn = sqrt(Double(n!))
+        
+        var maxIntArray = Array(repeating: true, count: Int(n!))
+        
+        for i in 2..<n!{
+            if (i%n!==0) {
+                maxIntArray[i] = false
+            }
+        }
+        
+        for item in maxIntArray {
+            
+                print(item)
+            
+            
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
