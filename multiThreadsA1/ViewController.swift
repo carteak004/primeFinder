8d67c5370217525845485277b20872ed6a9037b0
ta <ta@nius-mac-mini.local>
2017-09-05 16:47:28 -0500

    Initial Commit

---
+++ multiThreadsA1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  multiThreadsA1
+//
+//  Created by ta on 9/5/17.
+//  Copyright © 2017 ta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
